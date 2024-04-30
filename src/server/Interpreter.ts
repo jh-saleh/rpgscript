f103397a00d55eb87cd555346a15af7f36170478
jh-saleh <salehjh746@gmail.com>
2024-04-30 22:09:49 +0200

    code refactoring into class

--- src/server/Interpreter.ts
+++ src/server/Interpreter.ts
@@ -2,148 +2,169 @@ import * as fs from 'fs';
 import { Entities, Variable, almostFightSection, attack, counter, criticalHit, dodge, enter, fightSection, heal, healFor, isNumber, lose, special, variable } from './tokens';
 import { EntitiesError, FightError, FormatEnum } from './Errors';
 
-const getEntities = (instructions: string[], line: number): Record<string, Variable> => {
-    if (instructions[0] !== "Entities") {
-        throw Error(EntitiesError.EntitiesSectionMissing);
-    }
-    instructions.shift();
-    let entities: Record<string, Variable> = {};
-    let analyzing = true;
-    while (analyzing) {
-        line++;
-        const instr: string | undefined = instructions.shift();
-        if (instr === undefined) {
-            return entities;
-        }
-        if (fightSection.test(instr)) {
-            return entities;
-        } else {
-            if (almostFightSection.test(instr)) {
-                throw Error(FormatEnum(FightError.FightSectionSyntax, line.toString(), instr));
-            }
-        }
-        if (!variable.test(instr)) {
-            throw Error(FormatEnum(EntitiesError.WrongVaribleSyntax, line.toString()));
-        }
-        const entitySection: string[] = instr.split(":");
-        const entityData: string = entitySection[1].split(" ")[1];
-        if (entities.hasOwnProperty(entitySection[0])) {
-            throw Error(FormatEnum(EntitiesError.DuplicatedVariable, line.toString(), instr));
-        }
-        entities[entitySection[0]] = {
-            type: entityData.slice(entityData.length - 2, entityData.length) === Entities.hp ? "number" : "string",
-            value: Number(entityData.slice(0, entityData.length - 2)),
-            enteredCombat: false
-        };
-    }
-    return entities;
-}
-
-const fight = (entries: Record<string, Variable>, instructions: string[], consoleLogs: (string | number)[], line: number) => {
-    while (instructions.length > 0) {
-        line++;
-        const instr: string | undefined = instructions.shift();
-        if (instr === undefined) {
-            break;
-        }
-        const variables = extractVariableFromInstruction(instr, entries, line);
-        let didVariablesEnterCombat = false;
-        if (enter.regExp.test(instr)) {
-            for (let variable of variables) {
-                entries[variable].enteredCombat = true;
-            }
-            didVariablesEnterCombat = true;
-        }
-        if (!didVariablesEnterCombat) {
-            const areTheVariablesNotProtected = variables
-                .filter((value) => !isNumber.test(value))
-                .map((value) => entries[value].enteredCombat)
-                .reduce((previousValue, currentValue) => currentValue && previousValue);
-            if (areTheVariablesNotProtected) {
-                if (attack.regExp.test(instr)) {
-                    entries[variables[1]].value -= entries[variables[0]].value;
-                } else if (lose.regExp.test(instr)) {
-                    entries[variables[0]].value -= Number(variables[1]);
-                } else if (heal.regExp.test(instr)) {
-                    entries[variables[1]].value += entries[variables[0]].value;
-                } else if (healFor.regExp.test(instr)) {
-                    entries[variables[0]].value += Number(variables[1]);
-                } else if (criticalHit.regExp.test(instr)) {
-                    entries[variables[1]].value /= entries[variables[0]].value;
-                } else if (dodge.regExp.test(instr)) {
-                    entries[variables[1]].value *= entries[variables[0]].value;
-                } else if (counter.regExp.test(instr)) {
-                    const entity = variables[0];
-                    consoleLogs.push(entries[entity].type === "string" ? String.fromCharCode(entries[entity].value) : entries[entity].value);
-                } else {
-                    throw Error(FormatEnum(FightError.Syntax, line.toString(), instr));
-                }
-            } else {
-                throw Error(FightError.ProtectedEntity);
-            }
-        }
-    }
-}
-
-const findNbArgumentsForInstruction = (instr: string, line: number): number => {
-    if (enter.regExp.test(instr)) {
-        return enter.nbArguments;
-    } else if (attack.regExp.test(instr)) {
-        return attack.nbArguments;
-    } else if (lose.regExp.test(instr)) {
-        return lose.nbArguments;
-    } else if (heal.regExp.test(instr)) {
-        return heal.nbArguments;
-    } else if (healFor.regExp.test(instr)) {
-        return healFor.nbArguments;
-    } else if (criticalHit.regExp.test(instr)) {
-        return criticalHit.nbArguments;
-    } else if (dodge.regExp.test(instr)) {
-        return dodge.nbArguments;
-    } else if (counter.regExp.test(instr)) {
-        return counter.nbArguments;
-    } else {
-        throw Error(FormatEnum(FightError.Syntax, line.toString(), instr));
-    }
-}
-
-const extractVariableFromInstruction = (instr: string, variable: Record<string, Variable>, line: number): string[] => {
-    const reg = new RegExp("(" + Object.keys(variable).reduce((previousValue, currentValue) => previousValue + "|" + currentValue) + "|[1-9][0-9]*)", "g");
-    const nbArguments = findNbArgumentsForInstruction(instr, line);
-    const variablesExtracted = instr.match(reg)?.map((value) => value) ?? [];
-    if (variablesExtracted.length < nbArguments || (nbArguments === -1 && variablesExtracted.length < 1)) {
-        throw Error(FormatEnum(FightError.UnknownVariable, line.toString()));
-    }
-    return variablesExtracted;
-}
-
 interface InterpreterOutput {
-    consoleLogs: (string | number)[];
+    logs: (string | number)[];
     instructions: string[];
-    entities: Record<string, Variable>;
+    entries: Record<string, Variable>;
 }
 
-export const interprete = (path?: string, data?: string[]): InterpreterOutput => {
-    let line = -1;
-    let file: string = "";
-    let instructions: string[] = [];
-    if (path !== undefined) {
-        const pathSplit = path.split(".");
-        if (pathSplit[pathSplit.length - 1] !== "rpg") {
-            throw Error("Wrong extension file.");
-        }
-        file = fs.readFileSync(path).toString();
-        instructions = file.split('\n').filter(instr => !special.includes(instr));
-    } else if (data !== undefined) {
-        instructions = data;
-    } else {
-        throw Error("A path or data is required.");
-    }
-    const consoleLogs: (string | number)[] = [];
-    const entities = getEntities(instructions, line);
-    fight(entities, instructions, consoleLogs, line);
+export class Interpreter {
+    pc: number = 0;
+    rc: number = 0;
+    instructions: string[] = [];
+    entries: Record<string, Variable> = {};
+    logs: (string | number)[] = [];
 
-    //console.log("entities", entities);
-    //console.log("consoleLogs", consoleLogs);
-    return { consoleLogs, entities, instructions };
+    constructor() {
+        this.pc = 0;
+        this.rc = 0;
+        this.instructions = [];
+        this.entries = {};
+        this.logs = [];
+    }
+
+    reset() {
+        this.pc = 0;
+        this.rc = 0;
+        this.instructions = [];
+        this.entries = {};
+        this.logs = [];
+    }
+
+    extractVariables(): void {
+        if (this.instructions[this.pc] !== "Entities") {
+            throw Error(EntitiesError.EntitiesSectionMissing);
+        }
+        let entries: Record<string, Variable> = {};
+        let analyzing = true;
+        while (analyzing) {
+            this.pc++;
+            const instr: string = this.instructions[this.pc];
+            if (fightSection.test(instr)) {
+                this.entries = entries;
+                analyzing = false;
+                break;
+            } else {
+                if (almostFightSection.test(instr)) {
+                    throw Error(FormatEnum(FightError.FightSectionSyntax, this.pc.toString(), instr));
+                }
+            }
+            if (!variable.test(instr)) {
+                throw Error(FormatEnum(EntitiesError.WrongVaribleSyntax, this.pc.toString()));
+            }
+            const entitySection: string[] = instr.split(":");
+            const entityData: string = entitySection[1].split(" ")[1];
+            if (entries.hasOwnProperty(entitySection[0])) {
+                throw Error(FormatEnum(EntitiesError.DuplicatedVariable, this.pc.toString(), instr));
+            }
+            entries[entitySection[0]] = {
+                type: entityData.slice(entityData.length - 2, entityData.length) === Entities.hp ? "number" : "string",
+                value: Number(entityData.slice(0, entityData.length - 2)),
+                enteredCombat: false
+            };
+        }
+        this.entries = entries;
+    }
+
+    analyze() {
+        let interpreting = true;
+        while (interpreting) {
+            this.pc++;
+            const instr: string | undefined = this.instructions[this.pc];
+            if (instr === undefined) {
+                break;
+            }
+            const variables = this.extractVariableFromInstruction(instr);
+            let didVariablesEnterCombat = false;
+            if (enter.regExp.test(instr)) {
+                for (let variable of variables) {
+                    this.entries[variable].enteredCombat = true;
+                }
+                didVariablesEnterCombat = true;
+            }
+            if (!didVariablesEnterCombat) {
+                const areTheVariablesNotProtected = variables
+                    .filter((value) => !isNumber.test(value))
+                    .map((value) => this.entries[value].enteredCombat)
+                    .reduce((previousValue, currentValue) => currentValue && previousValue);
+                if (areTheVariablesNotProtected) {
+                    if (attack.regExp.test(instr)) {
+                        this.entries[variables[1]].value -= this.entries[variables[0]].value;
+                    } else if (lose.regExp.test(instr)) {
+                        this.entries[variables[0]].value -= Number(variables[1]);
+                    } else if (heal.regExp.test(instr)) {
+                        this.entries[variables[1]].value += this.entries[variables[0]].value;
+                    } else if (healFor.regExp.test(instr)) {
+                        this.entries[variables[0]].value += Number(variables[1]);
+                    } else if (criticalHit.regExp.test(instr)) {
+                        this.entries[variables[1]].value /= this.entries[variables[0]].value;
+                    } else if (dodge.regExp.test(instr)) {
+                        this.entries[variables[1]].value *= this.entries[variables[0]].value;
+                    } else if (counter.regExp.test(instr)) {
+                        const entity = variables[0];
+                        this.logs.push(this.entries[entity].type === "string" ? String.fromCharCode(this.entries[entity].value) : this.entries[entity].value);
+                    } else {
+                        throw Error(FormatEnum(FightError.Syntax, this.pc.toString(), instr));
+                    }
+                } else {
+                    throw Error(FightError.ProtectedEntity);
+                }
+            }
+        }
+    }
+
+    findNbArgumentsForInstruction(instr: string): number {
+        if (enter.regExp.test(instr)) {
+            return enter.nbArguments;
+        } else if (attack.regExp.test(instr)) {
+            return attack.nbArguments;
+        } else if (lose.regExp.test(instr)) {
+            return lose.nbArguments;
+        } else if (heal.regExp.test(instr)) {
+            return heal.nbArguments;
+        } else if (healFor.regExp.test(instr)) {
+            return healFor.nbArguments;
+        } else if (criticalHit.regExp.test(instr)) {
+            return criticalHit.nbArguments;
+        } else if (dodge.regExp.test(instr)) {
+            return dodge.nbArguments;
+        } else if (counter.regExp.test(instr)) {
+            return counter.nbArguments;
+        } else {
+            throw Error(FormatEnum(FightError.Syntax, this.pc.toString(), instr));
+        }
+    }
+
+    extractVariableFromInstruction(instr: string): string[] {
+        const reg = new RegExp("(" + Object.keys(this.entries).reduce((previousValue, currentValue) => previousValue + "|" + currentValue) + "|[1-9][0-9]*)", "g");
+        const nbArguments = this.findNbArgumentsForInstruction(instr);
+        const variablesExtracted = instr.match(reg)?.map((value) => value) ?? [];
+        if (variablesExtracted.length < nbArguments || (nbArguments === -1 && variablesExtracted.length < 1)) {
+            throw Error(FormatEnum(FightError.UnknownVariable, this.pc.toString()));
+        }
+        return variablesExtracted;
+    }
+
+    execute(path?: string, data?: string[]): InterpreterOutput {
+        this.reset();
+        let file: string = "";
+        if (path !== undefined) {
+            const pathSplit = path.split(".");
+            if (pathSplit[pathSplit.length - 1] !== "rpg") {
+                throw Error("Wrong extension file.");
+            }
+            file = fs.readFileSync(path).toString();
+            this.instructions = file.split('\n').filter(instr => !special.includes(instr));
+        } else if (data !== undefined) {
+            this.instructions = data;
+        } else {
+            throw Error("A path or data is required.");
+        }
+        this.extractVariables();
+        this.analyze();
+
+        //console.log("entities", entities);
+        //console.log("consoleLogs", consoleLogs);
+        return { logs: this.logs, entries: this.entries, instructions: this.instructions };
+    }
 }
--- src/server/test/Instructions.test.ts
+++ src/server/test/Instructions.test.ts
@@ -1,100 +1,117 @@
 import { expect, test } from '@jest/globals';
 import { EntitiesError, FightError, FormatEnum } from '../Errors';
-import { interprete } from '../Interpreter';
+import { Interpreter } from '../Interpreter';
 
 test('interprete_should_not_allow_wrong_syntax_for_variables', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/entitiesSectionMissing.rpg");
+        interpreter.execute("src/server/test/data/entitiesSectionMissing.rpg");
     }).toThrow(EntitiesError.EntitiesSectionMissing);
 });
 
 test('interprete_should_not_allow_wrong_syntax_for_variables', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/wrongVariableSyntax.rpg");
-    }).toThrow(FormatEnum(EntitiesError.WrongVaribleSyntax, "0"));
+        interpreter.execute("src/server/test/data/wrongVariableSyntax.rpg");
+    }).toThrow(FormatEnum(EntitiesError.WrongVaribleSyntax, "1"));
 });
 
 test('interprete_should_not_allow_duplicated_variables', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/duplicatedVariables.rpg");
-    }).toThrow(FormatEnum(EntitiesError.DuplicatedVariable, "1", "dragon: 100hp"));
+        interpreter.execute("src/server/test/data/duplicatedVariables.rpg");
+    }).toThrow(FormatEnum(EntitiesError.DuplicatedVariable, "2", "dragon: 100hp"));
 });
 
 test('interprete_should_not_be_able_to_use_a_variable_that_was_not_declared', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/unknownVariable.rpg");
-    }).toThrow(FormatEnum(FightError.UnknownVariable, "0", "ghost"));
+        interpreter.execute("src/server/test/data/unknownVariable.rpg");
+    }).toThrow(FormatEnum(FightError.UnknownVariable, "3", "ghost"));
 });
 
 test('interprete_should_allow_variables_to_change_their_value_when_the_instruction_"The token1, ... and tokenN enter combat!"_exists', () => {
-    const { consoleLogs, entities, instructions } = interprete("src/server/test/data/enterCombat.rpg");
-    expect(entities["dragon"].enteredCombat).toBe(true);
-    expect(entities["ghost"].enteredCombat).toBe(true);
-    expect(consoleLogs.length).toBe(0);
-    expect(instructions.length).toBe(0);
+    const interpreter = new Interpreter();
+    const { logs, entries, instructions } = interpreter.execute("src/server/test/data/enterCombat.rpg");
+    expect(entries["dragon"].enteredCombat).toBe(true);
+    expect(entries["ghost"].enteredCombat).toBe(true);
+    expect(logs.length).toBe(0);
+    expect(instructions.length).toBe(5);
 });
 
 test('interprete_should_not_allow_variables_to_change_their_value_when_the_instruction_"The token1, ... and tokenN enter combat!"_is_missing', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/emptyFight.rpg");
+        interpreter.execute("src/server/test/data/emptyFight.rpg");
     }).toThrow(FightError.ProtectedEntity);
 });
 
 test('interprete_should_require_a_fight_section_with_a_proper_name', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/correctFightSection.rpg");
+        interpreter.execute("src/server/test/data/correctFightSection.rpg");
     }).not.toThrow(FormatEnum(FightError.FightSectionSyntax));
 });
 
 test('interprete_should_throw_an_error_when_the_fight_section_has_an_improper_name', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/incorrectFightSection.rpg");
-    }).toThrow(FormatEnum(FightError.FightSectionSyntax, "2", "Fight &+-\"*"));
+        interpreter.execute("src/server/test/data/incorrectFightSection.rpg");
+    }).toThrow(FormatEnum(FightError.FightSectionSyntax, "3", "Fight &+-\"*"));
 });
 
 test('interprete_should_not_allow_unknown_syntax', () => {
+    const interpreter = new Interpreter();
     expect(() => {
-        interprete("src/server/test/data/unknownSyntax.rpg");
-    }).toThrow(FormatEnum(FightError.Syntax, "1", "The dragon bleepbloop."));
+        interpreter.execute("src/server/test/data/unknownSyntax.rpg");
+    }).toThrow(FormatEnum(FightError.Syntax, "5", "The dragon bleepbloop."));
 });
 
 test('interprete_should_allow_a_variable_to_decrease_its_value_when_the_instruction_"a attack[s] b."_exists', () => {
-    const { entities } = interprete("src/server/test/data/attack.rpg");
-    expect(entities["dragon"].value).toBe(100);
-    expect(entities["ghost"].value).toBe(0);
+    const interpreter = new Interpreter();
+    const { entries } = interpreter.execute("src/server/test/data/attack.rpg");
+    expect(entries["dragon"].value).toBe(100);
+    expect(entries["ghost"].value).toBe(0);
 });
 
 test('interprete_should_allow_a_variable_to_decrease_its_value_when_the_instruction_"a lose[s] c points."_exists', () => {
-    const { entities } = interprete("src/server/test/data/losePoints.rpg");
-    expect(entities["dragon"].value).toBe(90);
+    const interpreter = new Interpreter();
+    const { entries } = interpreter.execute("src/server/test/data/losePoints.rpg");
+    expect(entries["dragon"].value).toBe(90);
 });
 
 test('interprete_should_allow_a_variable_to_increase_its_value_when_the_instruction_"a heal[s] b."_exists', () => {
-    const { entities } = interprete("src/server/test/data/heals.rpg");
-    expect(entities["dragon"].value).toBe(100);
-    expect(entities["wolf"].value).toBe(150);
+    const interpreter = new Interpreter();
+    const { entries } = interpreter.execute("src/server/test/data/heals.rpg");
+    expect(entries["dragon"].value).toBe(100);
+    expect(entries["wolf"].value).toBe(150);
 });
 
 test('interprete_should_allow_a_variable_to_increase_its_value_when_the_instruction_"a heal[s] for c points."_exists', () => {
-    const { entities } = interprete("src/server/test/data/healsFor.rpg");
-    expect(entities["dragon"].value).toBe(150);
+    const interpreter = new Interpreter();
+
+    const { entries } = interpreter.execute("src/server/test/data/healsFor.rpg");
+    expect(entries["dragon"].value).toBe(150);
 });
 
 test('interprete_should_allow_a_variable_to_divide_its_value_when_the_instruction_"a critically hit[s] b."_exists', () => {
-    const { entities } = interprete("src/server/test/data/criticalHits.rpg");
-    expect(entities["wolf"].value).toBe(50);
-    expect(entities["dragon"].value).toBe(2);
+    const interpreter = new Interpreter();
+    const { entries } = interpreter.execute("src/server/test/data/criticalHits.rpg");
+    expect(entries["wolf"].value).toBe(50);
+    expect(entries["dragon"].value).toBe(2);
 });
 
 test('interprete_should_allow_a_variable_to_multiply_its_value_when_the_instruction_"a dodge[s] b."_exists', () => {
-    const { entities } = interprete("src/server/test/data/dodge.rpg");
-    expect(entities["wolf"].value).toBe(50);
-    expect(entities["dragon"].value).toBe(5000);
+    const interpreter = new Interpreter();
+    const { entries } = interpreter.execute("src/server/test/data/dodge.rpg");
+    expect(entries["wolf"].value).toBe(50);
+    expect(entries["dragon"].value).toBe(5000);
 });
 
 test('interprete_should_print_the_variable_s_value_when_the_instruction_"a activate[s] a counter!"_exists', () => {
-    const { consoleLogs, entities } = interprete("src/server/test/data/counter.rpg");
-    expect(entities["dragon"].value).toBe(65);
-    expect(entities["ghost"].value).toBe(103);
-    expect(consoleLogs).toEqual([65, "g"]);
+    const interpreter = new Interpreter();
+    const { logs, entries } = interpreter.execute("src/server/test/data/counter.rpg");
+    expect(entries["dragon"].value).toBe(65);
+    expect(entries["ghost"].value).toBe(103);
+    expect(logs).toEqual([65, "g"]);
 });
