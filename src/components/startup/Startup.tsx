da0cea9e32f11afdd7d41fc9d21b421fece8eaf8
jh-saleh <salehjh746@gmail.com>
2024-05-31 12:23:09 +0200

    condition startup delay

--- src/components/startup/Startup.tsx
+++ src/components/startup/Startup.tsx
@@ -6,11 +6,12 @@ import { Welcome } from "./welcome/Welcome";
 export const Startup = () => {
     const [page, setPage] = useState<number>(0);
     const [intervalValue, setIntervalValue] = useState<NodeJS.Timeout>();
+    const delay = Number(process.env.NEXT_PUBLIC_STARTUP_DELAY);
 
     useEffect(() => {
         setIntervalValue(() => setInterval(() => {
             setPage((page) => page + 1);
-        }, 100));
+        }, delay));
     }, []);
 
     useEffect(() => {
